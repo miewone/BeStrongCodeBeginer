e6439df52cdf1ecf45af0fc99ffa9bf33e6c4079
wongyunPark <wongyun.park@bespinglobal.com>
2023-06-04 19:40:00 +0900

    #16 피보나치 문제해결

--- wongyun/1003/1003.py
+++ wongyun/1003/1003.py
@@ -1,19 +1,15 @@
 T = int(input())
 
+dp = [[0]*2 for _ in range(41)] 
+# dp[i][0]은 i번째 피보나치 수에서 0이 호출되는 횟수, dp[i][1]은 1이 호출되는 횟수
+dp[0][0], dp[0][1] = 1, 0
+dp[1][0], dp[1][1] = 0, 1
 
 
-def fibonacci(result,n):
-    if n==0: 
-        result[0] += 1
-        return 0
-    if n==1: 
-        result[1] += 1
-        return 1
-    else:
-        return fibonacci(result,n-1)+fibonacci(result,n-2)
-    
 for _ in range(T):
-    result = [0,0]
-    fibonacci(result,int(input()))
-    print(result[0], end=' ')
-    print(result[1])
+    N = int(input())
+    for i in range(2, N+1):
+        dp[i][0] = dp[i-1][0] + dp[i-2][0]
+        dp[i][1] = dp[i-1][1] + dp[i-2][1]
+
+    print(dp[N][0], dp[N][1])
